6dc8d48a25cd94a48a81a5f48b9ddae02386f52f
kari <mr.kari.ikonen@gmail.com>
2014-04-29 19:55:29 +0300

    INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EngineTest::Application.config.secret_token = '8658db3ed1e756de1ae940f9f8955294a61b0fa4f236b8d3bbcc6ba2199900bf33cf9b8d7dcad3a9f4a80ca902b632040300aadac361bb9de73b74b9348f3e53'
